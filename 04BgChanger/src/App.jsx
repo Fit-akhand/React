f66fd104dc8a2aa300765175a2a3cb02874389f5
Akhand Raj Singh <akhandrajsingh30@gmail.com.com>
2025-01-21 19:27:58 +0530

    programe for change background color

--- 04BgChanger/src/App.jsx
+++ 04BgChanger/src/App.jsx
@@ -28,6 +28,11 @@ function App() {
         className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
         style={{backgroundColor:"purple"}}
         >Purple</button>
+        <button
+        onClick={()=> setcolor("orange")}
+        className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+        style={{backgroundColor:"orange"}}
+        >Orange</button>
       </div>
     </div>
    </div>
